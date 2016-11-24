5ec16b8253556c3250313832d71cfc1d54bd2ea2
Ale Bon <Utente1@172.23.154.245>
2016-11-24 15:39:37 +0100

    c

--- application/controllers/PublicController.php
+++ application/controllers/PublicController.php
@@ -3,16 +3,17 @@
 class PublicController extends Zend_Controller_Action
 {
     protected $_authService;
-    
+    protected $_form;
+
     public function init()
     {
          $this->_helper->layout->setLayout('main');
          $this->_authService = new Application_Service_Auth();
+         $this->view->loginForm = $this->getLoginForm();
     }
     
     public function indexAction()
     {
-        
     } 
     
     public function infoAction () 
@@ -20,7 +21,17 @@ class PublicController extends Zend_Controller_Action
         
     }
     
-    
+    protected function getLoginForm()
+    {
+        $urlHelper = $this->_helper->getHelper('url');
+        $this->_form = new Application_Form_Public_Auth_Login();
+        $this->_form->setAction($urlHelper->url(array(
+            'controller' => 'access',
+            'action' => 'authenticate'),
+            'default'
+        ));
+        return $this->_form;
+    }
           
     public function logoutAction()
     {
