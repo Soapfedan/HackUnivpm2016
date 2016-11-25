62e0d6c30684e17626ab9e42809e2620d06d5f3b
Alberto Giacomini <giacomini-alberto@alice.it>
2016-11-25 11:24:25 +0100

    s

--- application/models/User.php
+++ application/models/User.php
@@ -11,17 +11,17 @@ class Application_Model_User extends App_Model_Abstract
 	//Consumo
 	public function getConsSinglePlug($id)
     {
-         return $this->getResource('CONSUMO')->getConsSinglePlug($id);
+         return $this->getResource('Consumo')->getConsSinglePlug($id);
     }
     
         public function getAllConsumption()
     {
-         return $this->getResource('CONSUMO')->getAllConsumption();
+         return $this->getResource('Consumo')->getAllConsumption();
     }
 	
 	public function getSinglePlugCons($id)
     {
-         return $this->getResource('CONSUMO')->getSinglePlugCons($id);
+         return $this->getResource('Consumo')->getSinglePlugCons($id);
     }
 	
 	
@@ -30,62 +30,62 @@ class Application_Model_User extends App_Model_Abstract
     
     public function getAllTemplates()
     {
-         return $this->getResource('TEMPLATES')->getAllTemplates();
+         return $this->getResource('Templates')->getAllTemplates();
     }
     
     public function getSingleTemplate($id)
     {
-         return $this->getResource('TEMPLATES')->getSingleTemplate($id);
+         return $this->getResource('Templates')->getSingleTemplate($id);
     }
 	public function getSingleTemplateAll($id)
     {
-         return $this->getResource('TEMPLATES')->getSingleTemplateAll($id);
+         return $this->getResource('Templates')->getSingleTemplateAll($id);
     }
     
     //TempPriority
     
     public function getAllPriorities()
     {
-         return $this->getResource('TEMP_PRIORITY')->getAllPriorities();
+         return $this->getResource('TempPriority')->getAllPriorities();
     }
     
     public function getSinglePriority($id)
     {
-         return $this->getResource('TEMP_PRIORITY')->getSinglePriority($id);
+         return $this->getResource('TempPriority')->getSinglePriority($id);
     }
     
      public function insertPriority($newP)
     {
-         return $this->getResource('TEMP_PRIORITY')->insertPriority($newP);
+         return $this->getResource('TempPriority')->insertPriority($newP);
     }
      public function updatePr($pr,$id)
     {
-         return $this->getResource('TEMP_PRIORITY')->updatePr($pr,$id);
+         return $this->getResource('TempPriority')->updatePr($pr,$id);
     }
      public function deletePr($id)
     {
-         return $this->getResource('TEMP_PRIORITY')->deletePr($id);
+         return $this->getResource('TempPriority')->deletePr($id);
     }
     
     
     //Presa
     public function getPlug($id)
     {
-         return $this->getResource('PRESA')->getPlug($id);
+         return $this->getResource('Presa')->getPlug($id);
     }
     
     public function getAllPlugs()
     {
-         return $this->getResource('PRESA')->getAllPlugs();
+         return $this->getResource('Presa')->getAllPlugs();
     }
     
     public function updatePlug($plug,$id)
     {
-         return $this->getResource('PRESA')->updatePlug($plug,$id);
+         return $this->getResource('Presa')->updatePlug($plug,$id);
     }
     public function setStatus($id)
     {
-         return $this->getResource('PRESA')->setStatus($id);
+         return $this->getResource('Presa')->setStatus($id);
     }
 	
   
@@ -93,17 +93,17 @@ class Application_Model_User extends App_Model_Abstract
     //Utente
     public function getUserbyUsername($username)
     {
-         return $this->getResource('UTENTE')->getUserbyUsername($username);
+         return $this->getResource('Utente')->getUserbyUsername($username);
     }
 	public function getUser(){
-        return $this->getResource('UTENTE')->getUser();
+        return $this->getResource('Utente')->getUser();
     }
     public function updateUser($form){
-        return $this->getResource('UTENTE')->updateUser($form);
+        return $this->getResource('Utente')->updateUser($form);
     }
 	public function controllaPsw($username, $password)
 	{
-         if(count($this->getResource('UTENTE')->controllaPsw($username, $password)) == 1){
+         if(count($this->getResource('Utente')->controllaPsw($username, $password)) == 1){
          	return true;
          }else{
          	return false;
@@ -113,31 +113,31 @@ class Application_Model_User extends App_Model_Abstract
     //PlugSchema
     public function getSingleSchema($id)
     {
-         return $this->getResource('PLUGTEMP')->getSingleSchema($id);
+         return $this->getResource('PlugTemp')->getSingleSchema($id);
     }
 	public function getSchemaName()
 	{
-         return $this->getResource('PLUGTEMP')->getSchemaName();
+         return $this->getResource('PlugTemp')->getSchemaName();
     }
     public function insertSchema($newP)
     {
-         return $this->getResource('PLUGTEMP')->insertSchema($newP);
+         return $this->getResource('PlugTemp')->insertSchema($newP);
     }
     public function getMaxId()
     {
-         return $this->getResource('PLUGTEMP')->getMaxId();
+         return $this->getResource('PlugTemp')->getMaxId();
     }
     public function updateTemp($pr,$idtemp,$idplug)
     {
-         return $this->getResource('PLUGTEMP')->updateTemp($pr,$idtemp,$idplug);
+         return $this->getResource('PlugTemp')->updateTemp($pr,$idtemp,$idplug);
     }
     public function deleteTemp($id)
     {
-         return $this->getResource('PLUGTEMP')->deleteTemp($id);
+         return $this->getResource('PlugTemp')->deleteTemp($id);
     }
     public function getAllSchema()
     {
-         return $this->getResource('PLUGTEMP')->getAllSchema();
+         return $this->getResource('PlugTemp')->getAllSchema();
     }
     
     
